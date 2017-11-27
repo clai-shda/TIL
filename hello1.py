de2384275d381f7b0341a10dd8fd42a88a4c9c54
clai-shda <clai.shda@gmail.com>
2017-11-27 14:32:29 +0900

    test_171127

---
+++ hello1.py
@@ -0,0 +1,17 @@
+image = [[1,2,3,4,5],
+         [5,4,3,2,1],
+        [10,20,30,40,50]]
+label = [10, 20, 30, 40, 50]
+
+ph_image = tf.placeholder(dtype=tf.float32)
+ph_label = tf.placeholder(dtype=tf.float32)
+ph3 = tf.placeholder(dtype=tf.float32)
+
+feed_dict = {ph_image: image, ph_label:label}
+
+result_tensor = ph_image + ph_label
+
+sess = tf.Session()
+result = sess.run(result_tensor, feed_dict=feed_dict)
+print(result)
+
